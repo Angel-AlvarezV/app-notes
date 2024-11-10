6d622eaaceecab1585e2fa2f25fbd8a6401dd13c
Angel <isc.angel.alvarez@gmail.com>
2024-11-09 21:24:13 -0800

    feat: Se agrega que el proyecto muestra la ruta raíz "http://localhost:3000/" en lugar de "http://localhost:3000/notes" al igual que todas las rutas de /notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :notes
-  root to: redirect("notes")
+  # Define las rutas manualmente para que estén en la raíz
+  root "notes#index"
+  get "/new", to: "notes#new", as: :new_note
+  get "/:id", to: "notes#show", as: :note
+  get "/:id/edit", to: "notes#edit", as: :edit_note
+  patch "/:id", to: "notes#update"
+  delete "/:id", to: "notes#destroy"
+  post "/", to: "notes#create", as: :notes
 end
